50b2ad30cd7919b2b4b0e7c5087d9b40fc88a646
MUHAMMAD 'ISMAH HAZIQ ISMAIL <ismahismail111@gmail.com>
2021-09-28 16:34:17 +0800

    Create footer.php

---
+++ admin/partials/footer.php
@@ -0,0 +1,9 @@
+        <!-- Footer Section Start -->
+        <div class="footer">
+            <div class="wrapper">
+                <p class="text-center">2021 All Right Reserved, Food House. Developed by - <a href="#">Ismah Ismail</a> </p>
+            </div>
+        </div>
+        <!-- Footer Section Ends -->
+    </body>
+</html>
